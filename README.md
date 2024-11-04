b9252dd9dd44448f85bf402c2f50afcef0cbaf19
menisadi <menisadi@gmail.com>
2024-11-04 13:52:44 +0200

    added now playing cover photo
    and readme

---
+++ README.md
@@ -0,0 +1,32 @@
+My Music Dashboard
+================
+
+**Under Construction**
+
+A Streamlit app that displays my Last.fm music dashboard.
+
+**Features**
+
+* Displays total play count
+* Shows currently playing track
+* Displays top 3 artists and genres for the selected time period (weekly, monthly, yearly, or overall)
+
+**Usage**
+
+* Clone the repository
+* Create a `tokens.json` file with your Last.fm API credentials (see example below)
+* Run the app with `streamlit run app.py`
+
+**tokens.json example**
+
+```json
+{
+    "last_api_key": "YOUR_API_KEY",
+    "last_secret": "YOUR_API_SECRET",
+    "last_username": "YOUR_LAST_FM_USERNAME",
+    "last_password": "YOUR_LAST_FM_PASSWORD"
+}
+```
+
+**Note**: This app is still in development. Functionality and design are subject to change.
+
--- app.py
+++ app.py
@@ -46,16 +46,31 @@ st.sidebar.markdown("Made by [meni](https://github.com/menisadi)")
 # Display current playing track
 now_playing = network.get_user(USERNAME).get_now_playing()
 if now_playing:
-    st.subheader("Now Playing")
-    st.write(f"🎵 {now_playing.artist} - {now_playing.title}")
-else:
-    st.subheader("Now Playing")
-    st.write("No song is currently playing.")
+    track = network.get_track(now_playing.artist, now_playing.title)
+    album = now_playing.get_album().get_name()
+    album_image_url = now_playing.get_cover_image()
+
+    col1, col2 = st.columns([1, 3])  # Adjust the proportions as needed
+
+    with col1:
+        if album_image_url:
+            st.image(
+                album_image_url, caption="", width=100
+            )  # Adjust width for smaller image
+        else:
+            st.write("")  # Fallback in case there is no image
+
+    with col2:
+        st.markdown(f"**Artist**: {now_playing.artist}")
+        st.markdown(f"**Album**: {album}")
+        st.markdown(f"**Title**: {now_playing.title}")
 
 
 def get_top_artists(period, limit=3):
     top_artists = network.get_user(USERNAME).get_top_artists(period=period, limit=limit)
-    top_artists_names = [(artist.item.name, artist.weight) for artist in top_artists]
+    top_artists_names = [
+        (artist.item.name, int(artist.weight)) for artist in top_artists
+    ]
 
     return top_artists_names
 
@@ -67,9 +82,9 @@ def get_top_genres(top_artists, limit=3, prune_tag_list=3):
         top_genres = network.get_artist(artist).get_top_tags(limit=prune_tag_list)
         for genre in top_genres:
             if genre.item.name in tags:
-                tags[genre.item.name] += int(genre.weight) * int(artist_weight)
+                tags[genre.item.name] += int(genre.weight) * artist_weight
             else:
-                tags[genre.item.name] = int(genre.weight) * int(artist_weight)
+                tags[genre.item.name] = int(genre.weight) * artist_weight
 
     # return the top genres in descending order of weight
     return sorted(tags.items(), key=lambda x: x[1], reverse=True)[:limit]
@@ -79,9 +94,13 @@ def get_top_genres(top_artists, limit=3, prune_tag_list=3):
 st.subheader("Top 3 Artists")
 top_artists = get_top_artists(time_period, limit=3)
 for artist, weight in top_artists:
-    st.write(f"{artist} - {weight}")
+    # st.write(f"{artist} - {weight}")
+    st.write(f"**{artist}**")
+    st.progress(int(weight / max([w for _, w in top_artists]) * 100))
 
 # Display top 3 genres
 st.subheader("Top 3 Genres")
-for genre, weight in get_top_genres(top_artists, limit=3, prune_tag_list=3):
-    st.write(f"{genre} - {weight}")
+top_genres = get_top_genres(top_artists, limit=3, prune_tag_list=3)
+for genre, weight in top_genres:
+    st.write(f"**{genre}**")
+    st.progress(int(weight / max([w for _, w in top_genres]) * 100))
