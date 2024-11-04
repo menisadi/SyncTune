e991729416ac248d55fcc7f90b8e0703b41183b9
menisadi <menisadi@gmail.com>
2024-11-04 19:37:30 +0200

    tags word cloud

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from typing import List, Tuple
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 from collections import Counter
 import pylast
 import streamlit as st
@@ -37,21 +40,21 @@ st.set_page_config(
     initial_sidebar_state="auto",
 )
 
-st.title("My Music Dashboard")
+st.title("TuneSync")
 
-# add a subtitle with total play count
 total_count = network.get_user(USERNAME).get_playcount()
-st.subheader(f"Total Play Count: {total_count}")
+st.write(f"Total Play Count: {total_count}")
 
 # Sidebar options for time periods
 chosen_time_period = st.sidebar.selectbox(
-    "Select Time Period", ["Weekly", "Monthly", "Yearly", "Overall"], index=3
+    "Select Time Period", ["Weekly", "Monthly", "Yearly", "Overall"], index=0
 )
 time_period = period_dict.get(chosen_time_period)
 st.header(f"{chosen_time_period} data")
 
 st.sidebar.markdown("---")
-st.sidebar.markdown("Made by [meni](https://github.com/menisadi)")
+st.sidebar.markdown("Made by [Meni](https://github.com/menisadi)")
+st.sidebar.markdown("Powered by [Last.fm](https://www.last.fm/)")
 
 # Display current playing track
 now_playing = network.get_user(USERNAME).get_now_playing()
@@ -76,6 +79,23 @@ if now_playing:
         st.markdown(f"**Title**: {now_playing.title}")
 
 
+def display_wordcloud(genres: List[Tuple[str, float]]):
+    genres_dict = dict(genres)
+    wordcloud = WordCloud(
+        width=400,
+        height=400,
+        background_color="#282a36",
+        contour_color="#bd93f9",
+        contour_width=3,
+    ).generate_from_frequencies(genres_dict)
+
+    # plt.figure(figsize=(3, 3))
+    # plt.imshow(wordcloud, interpolation="bilinear")
+    # plt.axis("off")
+    # st.pyplot(plt.gcf())
+    st.image(wordcloud.to_array())
+
+
 def get_top_artists(period, limit=3):
     top_artists = network.get_user(USERNAME).get_top_artists(period=period, limit=limit)
     top_artists_names = [
@@ -85,8 +105,14 @@ def get_top_artists(period, limit=3):
     return top_artists_names
 
 
-def get_top_genres(top_artists, limit=3, prune_tag_list=3):
-    # initiate a list to store the top genres
+def normalize_weights(tags: List[Tuple[str, int]]) -> List[Tuple[str, float]]:
+    max_weight = max([w for _, w in tags])
+    return [(t, w / max_weight) for t, w in tags]
+
+
+def get_top_genres(
+    top_artists: List[Tuple[str, int]], limit: int = 0, prune_tag_list: int = 0
+) -> List[Tuple[str, float]]:
     tags = dict()
     for artist, artist_weight in top_artists:
         top_genres = network.get_artist(artist).get_top_tags(limit=prune_tag_list)
@@ -96,17 +122,27 @@ def get_top_genres(top_artists, limit=3, prune_tag_list=3):
             else:
                 tags[genre.item.name] = int(genre.weight) * artist_weight
 
-    # return the top genres in descending order of weight
-    return sorted(tags.items(), key=lambda x: x[1], reverse=True)[:limit]
+    normalized_tags = normalize_weights(list(tags.items()))
+    if limit == 0:
+        return normalized_tags
+    else:
+        # return the top genres in descending order of weight
+        limited_tags = sorted(normalized_tags, key=lambda x: x[1], reverse=True)[:limit]
+        return limited_tags
 
 
 # Display top 3 artists
 st.subheader("Top 3 Artists")
 top_artists = get_top_artists(time_period, limit=3)
 for artist, weight in top_artists:
-    # st.write(f"{artist} - {weight}")
     st.write(f"**{artist}**")
     st.progress(int(weight / max([w for _, w in top_artists]) * 100))
+
+# Tags wordcloud
+more_top_artists = get_top_artists(time_period, limit=20)
+all_tags = get_top_genres(more_top_artists, limit=0, prune_tag_list=3)
+st.subheader("Word Cloud of Top Tags")
+display_wordcloud(all_tags)
 
 # Display top 3 genres
 st.subheader("Top 3 Genres")
