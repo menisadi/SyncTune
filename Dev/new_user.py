f2214a53ae044022974d26ab931fb8cc634459b3
menisadi <menisadi@gmail.com>
2024-11-18 09:59:00 +0200

    testing how to operate more than 1 user

---
+++ Dev/new_user.py
@@ -0,0 +1,24 @@
+import numpy as np
+from typing import List, Tuple
+from wordcloud import WordCloud, get_single_color_func
+import matplotlib.pyplot as plt
+from collections import Counter
+import pylast
+import streamlit as st
+import json
+
+
+with open("tokens.json", "r") as file:
+    tokens = json.load(file)
+
+API_KEY = tokens.get("last_api_key")
+API_SECRET = tokens.get("last_secret")
+USERNAME = tokens.get("last_username")
+PASSWORD_HASH = pylast.md5(tokens.get("last_password"))
+
+network = pylast.LastFMNetwork(
+    api_key=API_KEY,
+    api_secret=API_SECRET,
+    username=USERNAME,
+    password_hash=PASSWORD_HASH,
+)
